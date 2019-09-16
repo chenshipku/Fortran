1f091a50f2272cdbdf73b037d95a348916b70782
Electro Master <chenshipku@gmail.com>
2019-09-16 11:49:10 +0800

    Add files via upload

---
+++ .bat/doubleQQ.bat
@@ -0,0 +1,3 @@
+TASKKILL /F /IM QQScLauncher.exe
+start "" "E:\software\QQ\Bin\QQScLauncher.exe"
+start "" "E:\software\QQ\Bin\QQScLauncher.exe"
---
+++ .bat/doublewechat.bat
@@ -0,0 +1,3 @@
+TASKKILL /F /IM wechat.exe
+start "" "E:\software\WeChat\WeChat.exe"
+start "" "E:\software\WeChat\WeChat.exe"
